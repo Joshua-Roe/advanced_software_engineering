731f1ef6ff530092eb1e62ec2b153c83b415f052
joshua-roe <josh_roe@me.com>
2020-02-17 15:02:04 +0000

    added baggage frame

--- CheckInTerminal/src/GUI.java
+++ CheckInTerminal/src/GUI.java
@@ -4,9 +4,9 @@ class gui {
     public static void main(String args[]) {
 
         //create frame
-        JFrame frame = new JFrame("Check In");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 120);
+        JFrame checkFrame = new JFrame("Check In");
+        checkFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        checkFrame.setSize(400, 120);
 
         //Creating the panel at bottom and adding components
         JPanel namePanel = new JPanel(); // the panel is not visible in output
@@ -25,9 +25,39 @@ class gui {
         JButton confBtn= new JButton("Confirm");
         confPanel.add(confBtn);
         //Adding Components to the frame.
-        frame.getContentPane().add(BorderLayout.NORTH, namePanel);
-        frame.getContentPane().add(BorderLayout.CENTER, refPanel);
-        frame.getContentPane().add(BorderLayout.SOUTH, confPanel);
-        frame.setVisible(true);
+        checkFrame.getContentPane().add(BorderLayout.NORTH, namePanel);
+        checkFrame.getContentPane().add(BorderLayout.CENTER, refPanel);
+        checkFrame.getContentPane().add(BorderLayout.SOUTH, confPanel);
+        checkFrame.setVisible(true);
+        
+        
+        
+        
+        //create frame
+        JFrame bagFrame = new JFrame("Baggage");
+        bagFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        bagFrame.setSize(400, 120);
+
+        //Creating the panel at bottom and adding components
+        JPanel sizePanel = new JPanel(); // the panel is not visible in output
+        JLabel sizeLabel = new JLabel("Baggage Size");
+        JTextField sizeText = new JTextField(10); // accepts upto 10 characters
+        sizePanel.add(sizeLabel); // Components Added using Flow Layout
+        sizePanel.add(sizeText);
+        
+        JPanel weightPanel = new JPanel(); // the panel is not visible in output
+        JLabel weightLabel = new JLabel("Baggage Weight");
+        JTextField weightText = new JTextField(10); // accepts upto 10 characters
+        weightPanel.add(weightLabel); // Components Added using Flow Layout
+        weightPanel.add(weightText);
+        
+        JPanel conf2Panel = new JPanel(); // the panel is not visible in output        
+        JButton conf2Btn= new JButton("Confirm");
+        conf2Panel.add(conf2Btn);
+        //Adding Components to the frame.
+        bagFrame.getContentPane().add(BorderLayout.NORTH, sizePanel);
+        bagFrame.getContentPane().add(BorderLayout.CENTER, weightPanel);
+        bagFrame.getContentPane().add(BorderLayout.SOUTH, conf2Panel);
+        bagFrame.setVisible(true);
     }
 }
