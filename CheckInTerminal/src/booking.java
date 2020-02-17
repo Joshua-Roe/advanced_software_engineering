25b1e5fba705e303f2cce64bd67e62714786e815
Leo Chi Wai Kong <cwk2@hw.ac.uk>
2020-02-17 16:24:01 +0000

    booking class with all functions

--- CheckInTerminal/src/booking.java
+++ CheckInTerminal/src/booking.java
@@ -1,70 +1,45 @@
-import java.io.*; 
-import java.util.Random;
-import java.util.HashMap;
-import java.util.zip.CRC32;
-//import java.io.File;
-
-class Data{
-	public String FirstName; 
-    public String LastName;  
-    public String flightNumber;
-    //public String aircraft;
-    public boolean checkedIn;
-    //public float maxSize;
-    //public float maxWeight;
-    //public float currentSzie;
-    //public float currentWeight;
-    
+public class Booking
+{
+	private String bookingRef;
+	private String firstName;
+	private String lastName;
+	private String flightCode;
+	private boolean checkedIn;
+	private float baggageSize;
+	private float baggageWeight;
+	
+	public Booking(String brf, String fName, String lName, String fCode) 
+	{
+		this.bookingRef = brf;
+		this.firstName = fName;
+		this.lastName = lName;
+		this.flightCode = fCode;
+		this.checkedIn = false;
+	
+	}
+	
+	public String getReferenceCode() {
+		return bookingRef;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getflightCode() {
+		return flightCode;
+	}
+	
+	public boolean getCheckinStatus() {
+		return checkedIn;
+	}
+	
+	public void setCheckinStatus() {
+		checkedIn = true;
+	}
+	
+	public void setBaggageInfo(float bSize, float bWeight) {
+		this.baggageSize = bSize;
+		this.baggageWeight = bWeight;
+	}
 }
-
-public class booking { 
-	public static void main(String[] args)throws Exception{ 
-
-		File bookingCSV = new File("../../data/bookingDetails.csv"); 
-		File flightCSV = new File("../../data/flightDetails.csv");
-
-		//export.createNewFile();
-		
-		BufferedReader bookingBR = new BufferedReader(new FileReader(bookingCSV)); 
-		BufferedReader flightBR = new BufferedReader(new FileReader(flightCSV));
-		
-		HashMap<String, Data> booking = new HashMap<String, Data>();
-		  
-		String st; 
-		while ((st = bookingBR.readLine()) != null) {
-			String[] temp = st.split(",", 0);
-			//randomNum = rand.nextInt(((flightNO.length-1) - 0) + 1) + 0;
-			//temp[0] = temp[0].replace("\"", "");
-			//temp[1] = temp[1].replace("\"", "");
-			//String key = temp[0]+temp[1]+flightNO[randomNum];
-			//System.out.println(temp[0] + temp[1] + flightNO[randomNum]);
-			Data detailData = new Data();
-			detailData.FirstName = temp[1];
-			detailData.LastName = temp[2];
-			detailData.flightNumber = temp[3];
-			if (temp[4] == "N")
-				detailData.checkedIn = false;
-			else if (temp[4] == "Y")
-				detailData.checkedIn = true;
-			
-			booking.put(temp[0], detailData);
-			//System.out.println("input:"+detailData);
-
-			//CRC32 crc = new CRC32();
-	        //crc.update(key.getBytes());
-	        //long tempKey = crc.getValue();
-	        //bw.write(Long.toHexString(tempKey) + "," + temp[0] + "," + temp[1] + "," + flightNO[randomNum] + "," + "N");
-	        //bw.newLine();
-	        
-	        //System.out.println("CRC32:"+Long.toHexString(tempKey));
-			
-		}
-		bookingBR.close();
-		flightBR.close();
-		System.out.println("input:"+booking);
-		
-		
-		//System.out.println(bookingREF);
-			//System.out.println(st); 
-		} 
-} 
