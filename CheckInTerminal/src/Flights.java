84bf51911f0bdd71e2bfcd16c8e7c4f3a88c659c
maroux <mk28@hw.ac.uk>
2020-02-25 11:50:50 +0000

    adding Flights.java

---
+++ CheckInTerminal/src/Flights.java
@@ -0,0 +1,23 @@
+
+import java.io.*;
+import java.util.Random;
+import java.util.HashMap;
+import java.util.zip.CRC32;
+/*
+ * @author      Marek Kujawa
+ * @version     %I%, %G%
+ * @since       1.0
+ */
+package checkin_package;
+
+import checkin_package.*;
+
+public class Flights {
+    protected String airline;
+    protected String depart;
+    protected String destination;
+    protected String aircraft;
+    protected int capacity;
+    protected float max_size;   /*  max allowed size of the laugage */
+    protected float max_weight; /*  max allowed weight of the laugage */
+}
