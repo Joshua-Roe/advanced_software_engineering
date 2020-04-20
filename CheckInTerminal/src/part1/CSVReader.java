4adb1f52e9d1f98f602e9a99099ab0b5bdf126b0
Leo Chi Wai Kong <cwk2@hw.ac.uk>
2020-04-20 10:51:45 +0100

    Leo commented CSVGEN and CSVREADER

--- CheckInTerminal/src/part1/CSVReader.java
+++ CheckInTerminal/src/part1/CSVReader.java
@@ -5,115 +5,102 @@ import java.util.Random;
 
 import javax.swing.*;
 
+/*
+This is a class to read two CSV files and stores details to the hashmap
+This class also generate some random number for the size and weight of the baggage of each passenger
+This also check if the path and format are correct of these csv files
+*/
 public class CSVReader { 
 	private AllFlights flights;
 	private AllBookings bookings;
 	public CSVReader() {
 		try {
 		    String sep = File.separator;
-		    String bookingCSVpath = new File("data"+sep+"bookingDetails.csv").getAbsolutePath();
-		    String flightCSVpath = new File("data"+sep+"flightDetails.csv").getAbsolutePath();
+		    String bookingCSVpath = new File("data"+sep+"bookingDetails.csv").getAbsolutePath(); //the path of booking details csv
+		    String flightCSVpath = new File("data"+sep+"flightDetails.csv").getAbsolutePath(); //the path of flight details csv
 		   
-			File flightCSV = new File(flightCSVpath);
-			File bookingCSV = new File(bookingCSVpath);
-			BufferedReader bookingBR = new BufferedReader(new FileReader(bookingCSV)); 
-			BufferedReader flightBR = new BufferedReader(new FileReader(flightCSV));
+			File flightCSV = new File(flightCSVpath); //opeb the csv file 
+			File bookingCSV = new File(bookingCSVpath); //opeb the csv file 
+			BufferedReader bookingBR = new BufferedReader(new FileReader(bookingCSV)); //prepare to read the file 
+			BufferedReader flightBR = new BufferedReader(new FileReader(flightCSV)); //prepare to read the file 
 			
-			bookings = new AllBookings();
-			flights = new AllFlights();
+			bookings = new AllBookings(); //hashmap for booking details
+			flights = new AllFlights(); //hashmap for flight details
 			
 			String st; 
-			while ((st = bookingBR.readLine()) != null) {
-				String[] temp = st.split(",", 0);
-				Random rand1 = new Random();
-				Random rand2 = new Random();
-				Random rand3 = new Random();
-				Random rand4 = new Random();
-				float min = 1;
-				float maxWeight = 60;
-				float maxLH = 150;
-				float maxW = 100;
-				float ranWeight = rand1.nextFloat() * (maxWeight - min) + min;
+			while ((st = bookingBR.readLine()) != null) { //read the csv file until it reaches the end
+				String[] temp = st.split(",", 0); //splite the read line when there is a ","
+				Random rand1 = new Random(); //random number 1 for baggage 
+				Random rand2 = new Random(); //random number 2 for baggage
+				Random rand3 = new Random(); //random number 3 for baggage
+				Random rand4 = new Random(); //random number 4 for baggage
+				float min = 1; //this is the minimum random number of size or weight
+				float maxWeight = 60; //this is the maximum random number of wieght
+				float maxLH = 150; //this is the maximum random number of height and length
+				float maxW = 100;//this is the maximum random number of width
+				float ranWeight = rand1.nextFloat() * (maxWeight - min) + min; //the random weight is going to be between 1 and 60
 				ranWeight = Math.round(ranWeight*10.0f)/10.0f;
-				float ranL = rand2.nextFloat() * (maxLH - min) + min;
-				ranL = Math.round(ranL*10.0f)/10.0f;
-				float ranH = rand3.nextFloat() * (maxLH - min) + min;
-				ranH = Math.round(ranH*10.0f)/10.0f;
-				float ranW = rand4.nextFloat() * (maxW - min) + min;
-				ranW = Math.round(ranW*10.0f)/10.0f;
+				float ranL = rand2.nextFloat() * (maxLH - min) + min; //the random length is going to be between 1 and 150
+				ranL = Math.round(ranL*10.0f)/10.0f; //round the number 
+				float ranH = rand3.nextFloat() * (maxLH - min) + min; //the random height is going to be between 1 and 150
+				ranH = Math.round(ranH*10.0f)/10.0f; //round the number 
+				float ranW = rand4.nextFloat() * (maxW - min) + min; //the random width is going to be between 1 and 100
+				ranW = Math.round(ranW*10.0f)/10.0f; //round the number 
 				//Booking example = new Booking(bookingCode, firstName, lastName, flightCode, baggageWeight, baggageLength, baggageHeight, baggageWidth)
 				Booking booking = new Booking(temp[0], temp[1], temp[2], temp[3], ranWeight, ranL, ranH, ranW);
-				bookings.addBooking(booking);
+				bookings.addBooking(booking); //add to the hashmap
 			}
 			
-			while ((st = flightBR.readLine()) != null) {
-				String[] temp = st.split(",", 0);
+			while ((st = flightBR.readLine()) != null) { //read the csv file until it reaches the end
+				String[] temp = st.split(",", 0); //splite the read line when there is a ","
 				for(int i = 5; i<=10;i++){
-					try {
-						if(Integer.parseInt(temp[i])<0){
-							throw new NegativeValuesInCSVException("Value in column "+(i+1)+" of flightDetails.csv has a nagative value. Please do not tamper with CSV files.");
-						}
-					} catch (NumberFormatException e) {
-						throw new NumberFormatInCSVException("Value in column "+(i+1)+" of flightDetails.csv is not a number. Please do not tamper with CSV files.");
+					if(Integer.parseInt(temp[i])<0){ //check if there is any negative number
+						throw new NegativeValuesInCSVException("Value in column "+(i+1)+" of flightDetails.csv has a nagative value. Please do not tamper with CSV files.");
 					}
-					
 				}
-				String[] tempTime = temp[11].split(":");
-				int h = Integer.parseInt(tempTime[0]);
-        		int m = Integer.parseInt(tempTime[1]);
-				int time = h*60 + m;
+				String[] tempTime = temp[11].split(":"); //split the component again, and this is for the departure time
+				int h = Integer.parseInt(tempTime[0]); //change the string to int
+        		int m = Integer.parseInt(tempTime[1]); //change the string to int 
+				int time = h*60 + m; //change the time to minute format, eg. 02:00 to 120
 				//Flight example = new Flight(flightCode, destination, carrier, maxPassengers, allowedBaggageWeight, allowedBaggageLength, allowedBaggageHeight, allowedBaggageWidth, excessFeeCharge, time)
 				Flight flight = new Flight(temp[0], temp[3], temp[1], Integer.parseInt(temp[5]), Float.parseFloat(temp[6]), Float.parseFloat(temp[7]), Float.parseFloat(temp[8]), Float.parseFloat(temp[9]), Float.parseFloat(temp[10]), time);
-				flights.addFlight(flight);
+				flights.addFlight(flight); //add to the hashmap
 			}
 			
-			bookingBR.close();
-			flightBR.close();
+			bookingBR.close(); //close the file 
+			flightBR.close(); //close the file 
 		}
-		catch(FileNotFoundException e) {
+		catch(FileNotFoundException e) { //exeption of csv files cannot be found 
 			generatePopUp(e);
 		}
-		catch(NegativeValuesInCSVException e) {
+		catch(NegativeValuesInCSVException e) { //exception of negative numbers found from csv files
 			generatePopUp(e);
 		}
-		catch(NumberFormatInCSVException e) {
-			generatePopUp(e);
-		}
-		catch(IOException e) {
+		catch(IOException e) { //exception of  IOExceptions not handled by previous catch blocks
 			System.out.println("IO exception experienced.");
 		}
-
 	} 
 
-	public class NegativeValuesInCSVException extends RuntimeException {
+	public class NegativeValuesInCSVException extends RuntimeException { //check if there is any negative number from the csv file
         public NegativeValuesInCSVException(String errorMessage) {
             super(errorMessage);
         }
-	}
-	
-	public class NumberFormatInCSVException extends RuntimeException {
-        public NumberFormatInCSVException(String errorMessage) {
-            super(errorMessage);
-        }
     }
-	public AllBookings getBookings() {
+	public AllBookings getBookings() { //other class can call this function to get back the hashmap of booking details
 		return bookings;
 	}
 	
-	public AllFlights getFlights() {
+	public AllFlights getFlights() { //other class can call this function to back the hashmap of get flight details
 		return flights;
 	}
 
-	public void generatePopUp(Exception e){
+	public void generatePopUp(Exception e){ //pop up warning message when exception case
 		JFrame f = new JFrame();
 		String popupMessage = "Unknown exception when reading CSV";
 		if(e instanceof FileNotFoundException){
 			popupMessage = "Data folder not found, ensure it is in its original directory.";
 		}
-		else if(e instanceof NegativeValuesInCSVException){
-			popupMessage = e.getMessage();
-		}
-		else if(e instanceof NumberFormatInCSVException){
+		if(e instanceof NegativeValuesInCSVException){
 			popupMessage = e.getMessage();
 		}
 		JOptionPane.showMessageDialog(f, popupMessage, 
